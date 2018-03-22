3e7dfd2dd295b929e10fb2d6b207b291aa025d7e
Bogdan Kalinowski <mybodya@gmail.com>
2018-03-22 16:56:21 +0100

    receive all parameters - done

--- src/js/getEnvironmentVars.js
+++ src/js/getEnvironmentVars.js
@@ -1,35 +1,71 @@
-const language = navigator.language;
 const date = new Date();
 const time = date.toLocaleString(language, { hour: 'numeric', hour12: true });
+const language = navigator.language;
 const day = date.toLocaleString(language, {  weekday: 'long' });
-const os = navigator.platform;
 const screenWidth = window.innerWidth;
 const device = screenWidth < 756 ? 'Mobile' : (screenWidth < 1024 ? 'Tablet' : 'Desktop');
+const os = navigator.platform;
+var country = "",
+    city = "",
+    weather = "";
 
-const locationPromise = new Promise((resolve, reject) => {
+const getEnvironmentPromise = new Promise((resolve, reject) => {
   try {
-    const xhr = new XMLHttpRequest();
-    xhr.addEventListener('readystatechange', () => {
-      if (xhr.readyState === 4) {
-        debugger;
+    var xmlhttpLocation = new XMLHttpRequest();
+
+    xmlhttpLocation.onreadystatechange = function() {
+
+      if (this.readyState == 4 && this.status == 200) {
+
+        var locationResponse = JSON.parse(this.responseText);
+        city = locationResponse.city;
+        country = locationResponse.city;
+
+        try {
+          var xmlhttpWeather = new XMLHttpRequest();
+
+          xmlhttpWeather.onreadystatechange = function() {
+
+            if (this.readyState == 4 && this.status == 200) {
+
+              var weatherResponse = JSON.parse(this.response);
+              weather = "Temperature: " + weatherResponse.main.temp + ". " + weatherResponse.weather[0].main;
+              alert(weather);
+
+            }
+          };
+
+          xmlhttpWeather.open('GET', "http://api.openweathermap.org/data/2.5/weather?" +
+            "lat=" + locationResponse.lat + '&' +
+            "lon=" + locationResponse.lon + '&' +
+            "units=metric" + '&' +
+            "APPID=05233ff759e21c294b058dfe648b690b",
+            true);
+          xmlhttpWeather.send();
+        }
+        catch (e) {
+          reject(e);
+        }
 
         resolve({
-          language,
-          time,
-          day,
-          os,
-          device,
-        });
+                time,
+                day,
+                language,
+                device,
+                os,
+                country,
+                city,
+                weather
+              });
       }
-    });
+    };
 
-    xhr.open('GET', 'http://freegeoip.net/json/');
-    xhr.setRequestHeader('cache-control', 'no-cache');
-    xhr.send();
+    xmlhttpLocation.open('GET', 'http://ip-api.com/json/', true);
+    xmlhttpLocation.send();
   }
   catch (e) {
     reject(e);
   }
 });
 
-export default locationPromise;
+export default getEnvironmentPromise;
