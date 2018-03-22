05904eae1caa14a9f5b1e96360e7f3a68da70f47
Bohdan Kalynovskyi <mybodya@gmail.com>
2018-03-22 15:14:21 +0100

    rename params, export json as js file

--- src/js/dco.js
+++ src/js/dco.js
@@ -1,4 +1,4 @@
-{
+export default {
 	"creative": {
 		"language": {
 			"English": {
--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import getEnvVars from './js/getEnvironmentVars'
 
 import './main.css';
 
-import json from './js/dco.json';
+import json from './js/dco.js';
 
 
 class AdUnit extends Mads {
@@ -14,7 +14,7 @@ class AdUnit extends Mads {
   constructor() {
     super();
     getEnvVars.then(envVars => {
-      this.unpackedParams = jsonParamsExtractor(json, envVars);
+      this.params = jsonParamsExtractor(json, envVars);
       this.finalRender();
     });
   }
@@ -26,18 +26,18 @@ class AdUnit extends Mads {
   }
 
   finalRender() {
-    const backgroundNode = this.unpackedParams.creative.type === "image" ?
-      `<img id="ad-background" src="${this.unpackedParams.creative.url}" alt="">`
+    const backgroundNode = this.params.creative.type === "image" ?
+      `<img id="ad-background" src="${this.params.creative.url}" alt="">`
       :
       `<video id="ad-background" width="100%" height="100%" autoplay>
-        <source src="${this.unpackedParams.creative.url}">
+        <source src="${this.params.creative.url}">
       </video>`;
 
     document.getElementById('ad-container').innerHTML = `
       ${backgroundNode}
-      <h1 id="ad-heading">${this.unpackedParams.headline}</h1>
-      <p id="ad-description">${this.unpackedParams.description}</p>
-      <a id="ad-cta" href="${this.unpackedParams.cta.url}">${this.unpackedParams.cta.label}</a>
+      <h1 id="ad-heading">${this.params.headline}</h1>
+      <p id="ad-description">${this.params.description}</p>
+      <a id="ad-cta" href="${this.params.cta.url}">${this.params.cta.label}</a>
     `;
   }
 
